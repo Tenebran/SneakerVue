3b77567840d5e3ce62005323227f9f418d9a271f
Sergej Garkusha <Tenebrann@googlemail.com>
2024-12-15 21:35:01 +0100

    feat: add header layout and project assets (SVG icons and images)
    - Implemented header layout (markup only, no logic yet)
    - Added SVG icons and images required for the project

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Header from './components/Header.vue';
+</script>
 
-<template>hello +++</template>
+<template>
+  <div class="w-4/5 m-auto bg-white h-screen rounded-xl shadow-xl mt-14 mb-14">
+    <Header />
+  </div>
+</template>
 
 <style scoped></style>
