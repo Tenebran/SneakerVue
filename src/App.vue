c5ad1ca951b547274dcd616f5c48081663e56022
Sergej Garkusha <Tenebrann@googlemail.com>
2024-12-16 13:58:28 +0100

    feat: add CardList component for displaying sneaker cards

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
 import Header from './components/Header.vue';
-import Card from './components/Card.vue';
+import CardList from './components/CardList.vue';
 </script>
 
 <template>
-  <div class="w-4/5 m-auto bg-white h-screen rounded-xl shadow-xl mt-14 mb-14">
+  <div class="w-4/5 m-auto bg-white rounded-xl shadow-xl mt-14 mb-14">
     <Header />
-    <Card />
+    <CardList />
   </div>
 </template>
 
---
+++ src/components/CardList.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="p-10">
+    <h2 class="text-3xl font-bold mb-8">Все кросовки</h2>
+    <div class="grid grid-cols-4 gap-5">
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import Card from './Card.vue';
+</script>
+
+<style scoped></style>
