e95bc871053ed35a34d6b512a76fd641339a8cf2
Sergej Garkusha <Tenebrann@googlemail.com>
2024-12-16 17:51:52 +0100

    feat: add Drawer components for shopping cart functionality

---
+++ src/components/CardItem.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="border items-center flex border-slate-200 p-4 rounded-xl gap-4">
+    <img class="w-16 h-16" src="/sneakers/sneakers-1.jpg" />
+    <div class="flex flex-col justify-between">
+      <p>Кросовки Nike Air Max 270</p>
+      <div class="flex justify-between mt-2">
+        <b>12990 руб</b>
+        <img class="opacity-40 hover:opacity-100 cursor-pointer transition" src="/close.svg" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
